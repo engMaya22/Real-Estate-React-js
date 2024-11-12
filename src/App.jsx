c9abc1a3797fb476f3d915289d6604f9c659fb90
engMaya22 <eng.maya.esmaeel1@gmail.com>
2024-11-12 12:13:34 -0800

    header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Navbar from "./components/Navbar"
-
+import Header from "./components/Header"
 
 
 function App() {
 
   return (
     <>
-       <div className="">
-         <Navbar />
+       <div className="w-full overflow-hidden">
+         <Header />
 
        </div>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar"
+import {assets} from "../assets/assets"
+
+const Header = ( ) => {
+    return <div className="min-h-screen bg-cover mb-4 bg-center flex items-center w-full overflow-hidden"
+              style={{backgroundImage:"url('/header_img.png')"}} id="Header">
+                <Navbar /> 
+                <div className="container mx-auto text-center px-6 py-4  md:px-20 lg:px-32 text-white">
+                    <h2 className ="text-5xl md:text-6xl lg:text-[82px]
+                    max-w-3xl inline-block font-semibold pt-20 ">
+                        Explore homes that fit your dreams
+                    </h2>
+                    <div className="space-x-6 mt-16">
+                        <a href="#Projects" className="rounded border border-white py-3 px-8 "> Projects</a>
+                        <a href="#Contact" className="rounded border bg-blue-500 py-3 px-8"> Contact Us</a>
+
+                    </div>
+                </div>
+            </div>
+
+}
+export default Header;
