d5f2f24ae238e1d2dba66c804b2e8aa1d6cd9c6b
engMaya22 <eng.maya.esmaeel1@gmail.com>
2024-11-13 10:59:33 -0800

    mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import { assets } from "../assets/assets"
+import {useState ,useEffect} from "react"
 
 const Navbar = () => {
-    const {logo}  = assets;
+    const {logo , menu_icon ,cross_icon}  = assets;
+    const [ showMobileMenu , setShowMobileMenu] = useState(false);
+
+    useEffect(()=>{
+        if(showMobileMenu){
+            document.body.style.overflow = 'hidden'//stop scrolling in small screen menu
+
+        }else{
+             document.body.style.overflow = 'auto'
+        }
+        return ()=> document.body.style.overflow = 'auto'
+
+
+    },[showMobileMenu])
   return (
     <div className="w-full top-0 left-0 absolute z-10">
         <div className="container mx-auto flex justify-between items-center  py-4 px-6 md:px-20 lg:px-32 bg-transparent"> 
@@ -16,9 +30,26 @@ const Navbar = () => {
             <button className="hidden md:block bg-white px-8 py-2 rounded-full">
                 Signup
             </button>
-          
+            <img className="block md:hidden w-7 cursor-pointer" src={menu_icon}  onClick={()=>setShowMobileMenu(true)} />
         </div>
-      
+
+        {/* mobile-menu */}
+       
+            <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0' } right-0 top-0 bottom-0 overflow-hidden  bg-white transation-all duration-200` }>
+
+            <div className="flex justify-end p-6 cursor-pointer" onClick={()=>setShowMobileMenu(false)}>
+             <img src={cross_icon} className="w-6 " />
+            </div>
+            <ul className="flex flex-col mt-5 items-center gap-2 px-6 text-lg font-meduim">
+                <a href="#Home" className="inline-block px-4 py-2 rounded-full" onClick={()=>setShowMobileMenu(false)}>Home</a>
+                <a href="#About" className="inline-block px-4 py-2 rounded-full" onClick={()=>setShowMobileMenu(false)}>About</a>
+                <a href="#Projects" className="inline-block px-4 py-2 rounded-full" onClick={()=>setShowMobileMenu(false)}>Projects</a>
+                <a href="#Testimonials" className="inline-block px-4 py-2 rounded-full" onClick={()=>setShowMobileMenu(false)}>Testimonials</a>
+            </ul>
+           
+        </div>
+        
+       
     </div>
   )
 }
